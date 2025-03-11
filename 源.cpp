eb97421c12e52a6e1cd5149a7a53c552083d4579
MrHao1-del <3498297268@qq.com>
2025-03-11 17:26:30 +0800

    Update 源.cpp

--- 源.cpp
+++ 源.cpp
@@ -1,42 +1,42 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//
-// /*int MAX(int arr[], int n);
-//int main()
-//{
-//	int arr[] = { 2,5,1,6,8 };
-//	int n = sizeof(arr) / sizeof(int);
-//	int max = MAX(arr,n);
-//	printf("%d", max);
-//	return 0;
-//}*/
-////int MAX(int arr[],int n)
-////{
-////	int max = arr[0];
-////	for (int i = 0; i < n;i++)
-////	{
-////		if (arr[i] > max)
-////		{
-////			max = arr[i];
-////		}
-////	}
-////	return max;
-////}
-////T(n)=(n+1)/2=O(n),ËùÒÔÆ½¾ùÊ±¼ä¸´ÔÓ¶ÈÎªO£¨n£©
-//int highest_palce(int n);
-//int main()
-//{
-//	int n;
-//	scanf("%d", &n);
-//	int N=highest_palce(n);
-//	printf("×î¸ßÎ»ÊÇ£º%d", N);
-//	return 0;
-//}
-//int highest_palce(int n)
-//{
-//	while (n > 10)
-//	{
-//		n = n / 10;
-//	}
-//	return n;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+ int MAX(int arr[], int n);
+int main()
+{
+	int arr[] = { 2,5,1,6,8 };
+	int n = sizeof(arr) / sizeof(int);
+	int max = MAX(arr,n);
+	printf("%d", max);
+return 0;
+}
+int MAX(int arr[],int n)
+{
+	int max = arr[0];
+	for (int i = 0; i < n;i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	return max;
+}
+T(n)=(n+1)/2=O(n),æ‰€ä»¥å¹³å‡æ—¶é—´å¤æ‚åº¦ä¸ºOï¼ˆnï¼‰
+int highest_palce(int n);
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int N=highest_palce(n);
+	printf("æœ€é«˜ä½æ˜¯ï¼š%d", N);
+	return 0;
+}
+int highest_palce(int n)
+{
+	while (n > 10)
+	{
+		n = n / 10;
+}
+return n;
+}
